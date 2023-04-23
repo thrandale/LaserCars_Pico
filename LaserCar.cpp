f646bd3c35a1578d1be9fbc4aa49fef885c908c6
Theo Crandall <thrandale@gmail.com>
2023-04-23 17:07:44 -0600

    removed some test code

--- LaserCar.cpp
+++ LaserCar.cpp
@@ -2,42 +2,27 @@
 #include "pico/cyw43_arch.h"
 #include "pico/multicore.h"
 #include "pico/stdlib.h"
+#include "pico/util/queue.h"
 #include <stdio.h>
 
+#include "Adafruit_NeoPixel.h"
 #include "BTController.h"
 #include "Drive.h"
 #include "IRReceiver.h"
 #include "IRSender.h"
+#include "WeaponController.h"
+
+queue_t hitQueue;
+queue_t weaponQueue;
 
 // Core 1 runs the WeaponController (And subsequently the IR Receiver)
 void core1_entry()
 {
-    uint8_t rx_data = -1;
-    uint pins[] = {4, 5};
-    uint numPins = sizeof(pins) / sizeof(pins[0]);
-    IRReceiver receiver(pio1, pins, numPins);
+    WeaponController weaponController(&hitQueue, &weaponQueue);
 
     while (true)
     {
-        uint32_t *rx_frames = receiver.Receive();
-        for (int i = 0; i < numPins; i++)
-        {
-            if (rx_frames[i] != -1)
-            {
-                rx_data = receiver.Decode(rx_frames[i]);
-
-                if (rx_data != -1)
-                {
-                    printf("\t%d received: %02x", i, rx_data);
-                }
-                else
-                {
-                    printf("\t%d received: %08x", i, rx_frames[i]);
-                }
-            }
-        }
-
-        delete[] rx_frames;
+        weaponController.Run();
     }
 }
 
@@ -49,31 +34,13 @@ int main()
     // initialize the wifi chip
     cyw43_arch_init();
 
-    gpio_init(15);
-    gpio_init(16);
-    gpio_init(17);
-    gpio_init(22);
-    gpio_init(26);
+    // Initialize the queues
+    queue_init(&hitQueue, sizeof(queue_entry_t), 32);
+    queue_init(&weaponQueue, sizeof(queue_entry_t), 32);
+
+    // Start the second core
+    multicore_launch_core1(core1_entry);
 
     Drive::Init();
     BTController::Start();
-
-    while (true)
-    {
-        for (int i = 0; i < 6; i++)
-        {
-            gpio_put(15, 0);
-            gpio_put(16, 0);
-            gpio_put(17, 0);
-            int pin22 = gpio_get(22);
-            int pin26 = gpio_get(26);
-            printf("22: %d, 26: %d\n", pin22, pin26);
-        }
-
-        gpio_put(15, 1);
-        gpio_put(16, 0);
-        gpio_put(17, 0);
-        gpio_put(22, 0);
-        gpio_put(26, 0);
-    }
 }
